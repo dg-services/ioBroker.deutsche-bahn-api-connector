e26f6016714c61cdd20047657b792e279368f35b
Dominik Gerstner <coding@dg-services.de>
2020-10-22 09:24:01 -0700

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"deutsche-bahn-api-connector adapter settings": {
+		"en": "Adapter settings for deutsche-bahn-api-connector",
+		"de": "Adaptereinstellungen für deutsche-bahn-api-connector",
+		"ru": "Настройки адаптера для deutsche-bahn-api-connector",
+		"pt": "Configurações do adaptador para deutsche-bahn-api-connector",
+		"nl": "Adapterinstellingen voor deutsche-bahn-api-connector",
+		"fr": "Paramètres d'adaptateur pour deutsche-bahn-api-connector",
+		"it": "Impostazioni dell'adattatore per deutsche-bahn-api-connector",
+		"es": "Ajustes del adaptador para deutsche-bahn-api-connector",
+		"pl": "Ustawienia adaptera dla deutsche-bahn-api-connector",
+		"zh-cn": "deutsche-bahn-api-connector的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
